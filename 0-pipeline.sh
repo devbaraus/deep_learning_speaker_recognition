2a97a383ecf3584cd74253b2d57db85b8033fa85
devbaraus <me@baraus.dev>
2021-05-20 09:19:44 -0300

    .

--- 0-pipeline.sh
+++ 0-pipeline.sh
@@ -1,5 +1,7 @@
-# pip install lingvo gin tensorflow sklearn numpy=1.19.5
+# pip install lingvo gin gin-config tensorflow sklearn numpy=1.19.5 sklearn
 
 python 1-merge_audios.py
+python 2-process_mfcc.py
 python 3-process_leaf.py
 python 5-run_leaf_perceptron-svm.py
+python 6-run_grid_random_forest.py
